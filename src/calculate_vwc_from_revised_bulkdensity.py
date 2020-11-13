bfcd278995da5dab0dc730724d87321dc98a31b8
Bryan Carlson <bryan.r.carlson@gmail.com>
2020-11-13 15:09:08 -0800

    First pass at calculating vwc from gwc and revised bd data, have not proofed

--- src/calculate_vwc_from_revised_bulkdensity.py
+++ src/calculate_vwc_from_revised_bulkdensity.py
@@ -50,8 +50,9 @@ def getRevisedBulkDensity(
         bdPath,
         usecols=[0,1,2,3,4,5,7]
     )
-
-    return df
+    df_1998 = df[df["Year"] == 1998]
+    
+    return df_1998
 
 def transformBDPerHorizonToFoot(
     bdPerHorizon: pd.DataFrame
@@ -74,23 +75,43 @@ def transformBDPerHorizonToFoot(
     #       bd_df.TopDepthFt = i
     #       bd_df.BottomDepthFt = i + 1
 
-    bdPerFt = bdPerHorizon.copy()
-    bdPerFt = bdPerFt.assign(TopDepthFt = lambda x: x["TopDepth"] * CM_TO_FT)
-    bdPerFt = bdPerFt.assign(BottomDepthFt = lambda x: x["BottomDepth"] * CM_TO_FT)
+    bdPerHorizonFt = bdPerHorizon.copy()
+    bdPerHorizonFt = bdPerHorizonFt.assign(TopDepthFt = lambda x: x["TopDepth"] * CM_TO_FT)
+    bdPerHorizonFt = bdPerHorizonFt.assign(BottomDepthFt = lambda x: x["BottomDepth"] * CM_TO_FT)
 
     # Get samples for loop
     samples = bdPerHorizon[["Year", "ID2"]].drop_duplicates()
+    bdPerFt = pd.DataFrame()
 
     for index, row in samples.iterrows():
-        sample_df = bdPerFt.loc[(bdPerFt["Year"] == row["Year"]) & (bdPerFt["ID2"] == row["ID2"])]
+        sample_df = bdPerHorizonFt.loc[(bdPerHorizonFt["Year"] == row["Year"]) & (bdPerHorizonFt["ID2"] == row["ID2"])]
         for topDepth in TOP_DEPTHS:
             print(row["Year"], row["ID2"], topDepth)
             
-            sample_depth_df = sample_df.loc[(((sample_df["TopDepthFt"] >= topDepth) | (sample_df["BottomDepthFt"] >= topDepth)) & ((sample_df["TopDepthFt"] <= (topDepth + INCREMENT)) | (sample_df["BottomDepthFt"] <= (topDepth + INCREMENT))))]
-            print("sample_depth_df")
-            print(sample_depth_df)
-            sample_depth_df = sample_depth_df.apply(lambda x : calculateWeightedBulkDensity(x, topDepth, INCREMENT), axis = 1)
-            print(sample_depth_df)
+            sample_depth_df = sample_df.loc[
+                (((sample_df["TopDepthFt"] >= topDepth) | 
+                    (sample_df["BottomDepthFt"] >= topDepth)) & 
+                ((sample_df["TopDepthFt"] <= (topDepth + INCREMENT)) | 
+                    (sample_df["BottomDepthFt"] <= (topDepth + INCREMENT))))]
+            
+            if(len(sample_depth_df) > 0):
+                sample_depth_df = sample_depth_df.apply(
+                    lambda x : calculateWeightedBulkDensity(x, topDepth, INCREMENT), 
+                    axis = 1)
+
+                sum_of_weights = sample_depth_df["Weight"].sum()
+                if(sum_of_weights) != 1:
+                    msg = "WARNING: Sum of weights does not equal 1"
+                    sample_depth_df["Notes"] = msg if not set(["Notes"]).issubset(sample_depth_df) else (sample_depth_df["Notes"] + " | " + msg)
+                    #raise Exception("Sum of weights does not equal 1")
+
+                bulk_density_per_increment = sample_depth_df["BulkDensityWeighted"].sum()
+                if(bulk_density_per_increment):
+                    sample_depth_df["BulkDensityPerIncrement"] = bulk_density_per_increment
+                sample_depth_df["DepthIncrementTop"] = topDepth
+                sample_depth_df["DepthIncrementBottom"] = topDepth + INCREMENT
+                
+                bdPerFt = bdPerFt.append(sample_depth_df)
 
     return bdPerFt
 
@@ -103,23 +124,85 @@ def calculateWeightedBulkDensity(row, topDepth, increment):
     
     return row
 
+def cleanBDPerFootIntermediate(bdPerFoot):
+    df = bdPerFoot[["Year", "ID2", "Latitude", "Longitude", "DepthIncrementTop", "DepthIncrementBottom", "BulkDensityPerIncrement"]]
+    df = df.rename(columns={"DepthIncrementTop": "TopDepth", "DepthIncrementBottom": "BottomDepth", "BulkDensityPerIncrement": "BulkDensity"})
+    df = df.drop_duplicates()
+
+    return df
+
+def tidyGwcAggregate(gwcAggregate):
+    df = gwcAggregate.copy()
+
+    df = pd.melt(df,
+        ["ID2", "Year", "Season"],
+        var_name = "DepthLabel",
+        value_name = "GravimetricWaterContent")
+
+    df["BottomDepth"] = df.apply(lambda x: int(x["DepthLabel"].split("_")[1]), axis = 1)
+
+    df = df[["ID2", "Year", "Season", "BottomDepth", "GravimetricWaterContent"]]
+
+    return df
+
+def calculateVwcFromGwc(gwcTidy, bdPerFoot):
+    gwc = gwcTidy.copy()
+    bd = bdPerFoot.copy()
+
+    df = gwc.merge(bd, on = ["ID2", "BottomDepth"], how = "left")
+    df["VolumetricWaterContent"] = df.apply(lambda x: x["GravimetricWaterContent"] * x["BulkDensity"], axis = 1)
+
+    return df
+
 def main(
     pathRevisedBulkDensity: pathlib.Path,
-    pathGwcDir: pathlib.Path
+    pathGwcDir: pathlib.Path,
+    workingDir: pathlib.Path,
+    useCache: bool
 ):    
     ### Data Preparation
-    #getGravimetricWaterContent(pathGwcDir)
-    bulkDensity = getRevisedBulkDensity(pathRevisedBulkDensity)
-    bdPerFoot = transformBDPerHorizonToFoot(bulkDensity)
+    pathBulkDensityIntermediate = workingDir / "bulkDensityPerFootClean.csv"
+    pathGravimetricWCIntermediate = workingDir / "gravimetricWCAggregate.csv"
+    pathGravimetricWCTidy = workingDir / "gravimetricWCTidy.csv"
 
+    if(not useCache):
+        gwcAggregate = getGravimetricWaterContent(pathGwcDir)
+        gwcAggregate.to_csv(pathGravimetricWCIntermediate, index=False)
+        gwcTidy = tidyGwcAggregate(gwcAggregate)
+        gwcTidy.to_csv(pathGravimetricWCTidy, index=False)
+    else:
+        if(pathGravimetricWCIntermediate.is_file()):
+            gwcTidy = pd.read_csv(pathGravimetricWCTidy)
+        else:
+            raise Exception("Error finding cached file, run script with 'useCache' set to False")
+
+    if(not useCache):
+        bulkDensity = getRevisedBulkDensity(pathRevisedBulkDensity)
+        bdPerFootIntermediate = transformBDPerHorizonToFoot(bulkDensity)
+        bdPerFootIntermediate.to_csv(workingDir / "bulkDensityPerFootIntermediate.csv")
+        
+        bdPerFoot = cleanBDPerFootIntermediate(bdPerFootIntermediate)
+        bdPerFoot.to_csv(pathBulkDensityIntermediate, index=False)
+    else:
+        if(pathBulkDensityIntermediate.is_file()):
+            bdPerFoot = pd.read_csv(pathBulkDensityIntermediate)
+        else:
+            raise Exception("Error finding cached file, run script with 'useCache' set to False")
+    
+    df = calculateVwcFromGwc(gwcTidy, bdPerFoot)
+
+    df.to_csv(workingDir / "volumetricWaterContentFromRevisedBd.csv", index = False)
     
 if __name__ == "__main__":
     # parameters
     inputDir = pathlib.Path.cwd() / "input"
     inputRevisedBulkDensity = inputDir / "soilCore1998To2015ShallowDeepMergedByHorizon_20180926.csv"
     inputGwcDir = inputDir / "VwcSpringFallCalc"
+    workingDir = pathlib.Path.cwd() / "working"
     
     main(
         inputRevisedBulkDensity,
-        inputGwcDir
+        inputGwcDir,
+        workingDir,
+        True
     )
